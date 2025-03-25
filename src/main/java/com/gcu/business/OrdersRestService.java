8e55f38947b2e62f378f0bd1c65e3e5020c55584
sqiji <LoginGCU20@#>
2025-03-25 11:16:06 -0700

    Adding DevOPs logging

--- src/main/java/com/gcu/business/OrdersRestService.java
+++ src/main/java/com/gcu/business/OrdersRestService.java
@@ -2,6 +2,8 @@ package com.gcu.business;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gcu.model.OrderModel;
 
+
+
 @RestController
 @RequestMapping("/service") 
 public class OrdersRestService {
@@ -17,13 +21,17 @@ public class OrdersRestService {
 	@Autowired 
 	private OrdersBusinessServiceInterface service;
 	
+	private static final Logger logger = LoggerFactory.getLogger(OrdersRestService.class);
+	
 	@GetMapping(path="/getjson", produces= {MediaType.APPLICATION_JSON_VALUE})
 	public List<OrderModel> getOrderAsJson(){
+		logger.trace("==========> In getOrdersAsJson() at" + "/getjson");
 		return service.getOrders();
 	}
 	
 	@GetMapping(path="/getxml", produces= {MediaType.APPLICATION_ATOM_XML_VALUE})
 	public List<OrderModel> getOrderAsXml(){
+		logger.trace("==========> In getOrdersAsXML() at" + "/getxml");
 		return service.getOrders();
 	} 
 	
@@ -31,4 +39,6 @@ public class OrdersRestService {
 	public List<OrderModel> getOrders(){
 		return service.getOrders();
 	} 
+	
+	
 }
